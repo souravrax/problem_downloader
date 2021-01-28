4fe81c7a31aabbc5d35a4044f2d30120ef925616
Sourav Rakshit <rakshitsourav3@gmail.com>
2021-01-28 20:59:22 +0530

    Added CLA support

--- app.js
+++ app.js
@@ -1,64 +1,80 @@
 #!/usr/bin/env node
-const express = require("express");
-const fs = require("fs");
-const rimraf = require("rimraf");
-const chalk = require("chalk");
-const clear = require("clear");
-const ora = require('ora');
-const boxen = require("boxen");
+// npm packages import
+const express = require("express"); // express server
+const fs = require("fs"); // for file system
+const chalk = require("chalk"); // to colorize to the output
+const clear = require("clear"); // to clear the console
+const ora = require("ora"); // for animations
+const boxen = require("boxen"); // Output in a box
+const { Command } = require("commander"); // for command line args
+// const CLI = require('clui') // for animation (not used anymore)
 
-const settings = require("./settings.json");
-const testGenerator = require("./lib/generator");
-// const getProblemName = require("./lib/getProblemName");
+// Other imports
+const {
+    testCaseFolderName,
+    inputFileNamePrefix,
+    outputFileNamePrefix,
+    PORT,
+} = require("./settings.json");
+const { version, author, bin, description } = require("./package.json");
+const { testGenerator, directoryGenerator } = require("./lib/generator");
+const { yellow } = require("chalk");
 
+const program = new Command(); // creating a new program for command line arguments
+program.version(version); // setting the version of the program
+program.name("download");
+program.description(description);
+program
+    .option(
+        "-f, --folder <value>",
+        "Custom folder to keep the parsed test cases",
+        testCaseFolderName
+    )
+    .option(
+        "-i, --input <value>",
+        "File name prefix of the input file",
+        inputFileNamePrefix
+    )
+    .option(
+        "-o, --output <value>",
+        "File name prefix of the output file",
+        outputFileNamePrefix
+    )
+    .parse();
+
+// global variables
 const app = express();
 
-clear();
-app.use(express.json());
+const Spinner = ora({
+    text: chalk.gray.bold(`Waiting for Test Cases to arrive...`),
+    spinner: "dots12",
+});
+Spinner.color = 'yellow';
+const currentDirectory = process.cwd();
+const testCaseDirectory = currentDirectory + "/" + program.opts().folder;
 
-const PORT = settings.PORT;
+clear();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.status(200).send("Connection Established");
 });
 
-const {
-    testCaseFolderName,
-    inputFileNamePrefix,
-    outputFileNamePrefix,
-} = settings;
-const currentDirectory = process.cwd();
-const testCaseDirectory = currentDirectory + "/" + testCaseFolderName;
-
-// const CLI = require('clui')
-// const Spinner = new CLI.Spinner(
-//     chalk.blueBright.bold(`Listening for Test Cases ...`),
-//     ["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"]
-// );
-
-const Spinner = ora({
-    text: chalk.blueBright.bold(`Waiting for Test Cases to arrive...`),
-    spinner: 'dots12'
-});
-
 app.post("/", (req, res) => {
-    Spinner.stop();
-    let { name: problemName, tests: allTestCases, url } = req.body;
-    if (fs.existsSync(testCaseDirectory)) {
-        rimraf.sync(testCaseDirectory);
-    }
-    fs.mkdirSync(testCaseDirectory);
+    // Spinner.stop();
+    let { name, tests, url } = req.body;
+    Spinner.succeed(`Got ${tests.length} test cases!`);
+    directoryGenerator(testCaseDirectory);
     testGenerator(
-        allTestCases,
-        inputFileNamePrefix,
-        outputFileNamePrefix,
-        testCaseDirectory,
-        problemName
+        tests,
+        program.opts().input,
+        program.opts().output,
+        program.opts().folder,
+        name
     );
     Spinner.start();
 });
-
-const {version, author} = require('./package.json');
 
 app.listen(PORT, () => {
     console.log(
@@ -69,19 +85,35 @@ app.listen(PORT, () => {
                     top: 1,
                     left: 8,
                     right: 8,
-                    bottom: 1
+                    bottom: 1,
                 },
-                borderColor: 'yellowBright',
-                borderStyle: 'double',
+                borderColor: "yellowBright",
+                borderStyle: "double",
                 dimBorder: true,
                 margin: {
                     top: 0,
                     right: 0,
                     left: 0,
-                    bottom: 1
+                    bottom: 1,
                 },
                 backgroundColor: "black",
-                align: "center"
+                align: "center",
+            }
+        )
+    );
+    console.log(
+        boxen(
+            `folder: ${program.opts().folder}, input: ${
+                program.opts().input
+            }, output: ${program.opts().output}`,
+            {
+                padding: {
+                    left: 1,
+                    right: 1,
+                    top: 0,
+                    bottom: 0
+                },
+                borderStyle: "round"
             }
         )
     );
