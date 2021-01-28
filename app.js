293a75978179ae6f080cc0296c0de37c757af987
Sourav Rakshit <rakshitsourav3@gmail.com>
2021-01-28 19:41:34 +0530

    Structured and Changed the UI

--- app.js
+++ app.js
@@ -2,30 +2,44 @@
 const express = require("express");
 const fs = require("fs");
 const rimraf = require("rimraf");
-const app = express();
-const figlet = require("figlet");
 const chalk = require("chalk");
 const clear = require("clear");
-const CLI = require("clui");
-const terminalLink = require("terminal-link");
-const better = {};
-require("better-logging")(better);
+const ora = require('ora');
+const boxen = require("boxen");
+
+const settings = require("./settings.json");
+const testGenerator = require("./lib/generator");
+// const getProblemName = require("./lib/getProblemName");
+
+const app = express();
 
 clear();
 app.use(express.json());
 
-const PORT = 10046;
+const PORT = settings.PORT;
 
 app.get("/", (req, res) => {
     res.status(200).send("Connection Established");
 });
 
+const {
+    testCaseFolderName,
+    inputFileNamePrefix,
+    outputFileNamePrefix,
+} = settings;
 const currentDirectory = process.cwd();
-const testCaseDirectory = currentDirectory + "/test_cases";
-const Spinner = new CLI.Spinner(
-    chalk.blueBright.bold(`Listening for Test Cases ...`),
-    ["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"]
-);
+const testCaseDirectory = currentDirectory + "/" + testCaseFolderName;
+
+// const CLI = require('clui')
+// const Spinner = new CLI.Spinner(
+//     chalk.blueBright.bold(`Listening for Test Cases ...`),
+//     ["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"]
+// );
+
+const Spinner = ora({
+    text: chalk.blueBright.bold(`Waiting for Test Cases to arrive...`),
+    spinner: 'dots12'
+});
 
 app.post("/", (req, res) => {
     Spinner.stop();
@@ -34,31 +48,42 @@ app.post("/", (req, res) => {
         rimraf.sync(testCaseDirectory);
     }
     fs.mkdirSync(testCaseDirectory);
-    const link = terminalLink(problemName, url);
-
-    allTestCases.forEach(({ input, output }, idx) => {
-        better.log(
-            chalk`{green Creating Test Case} {yellowBright.bold ${
-                idx + 1
-            }} {green for problem} {yellowBright.bold ${problemName}}`
-        );
-        fs.writeFileSync(`${testCaseDirectory}/in${idx + 1}`, input, (err) => {
-            better.error(err);
-        });
-        fs.writeFileSync(
-            `${testCaseDirectory}/out${idx + 1}`,
-            output,
-            (err) => {
-                better.error(err);
-            }
-        );
-    });
+    testGenerator(
+        allTestCases,
+        inputFileNamePrefix,
+        outputFileNamePrefix,
+        testCaseDirectory,
+        problemName
+    );
     Spinner.start();
 });
 
+const {version, author} = require('./package.json');
+
 app.listen(PORT, () => {
-    console.log(chalk.white.bold(figlet.textSync("Test  Case\nGenerator", {
-        whiteSpaceBreak: true,
-    })));
+    console.log(
+        boxen(
+            chalk`{bold CP Problem Downloader} {yellowBright v${version}}\nby {blueBright ${author}}`,
+            {
+                padding: {
+                    top: 1,
+                    left: 8,
+                    right: 8,
+                    bottom: 1
+                },
+                borderColor: 'yellowBright',
+                borderStyle: 'double',
+                dimBorder: true,
+                margin: {
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    bottom: 1
+                },
+                backgroundColor: "black",
+                align: "center"
+            }
+        )
+    );
     Spinner.start();
 });
